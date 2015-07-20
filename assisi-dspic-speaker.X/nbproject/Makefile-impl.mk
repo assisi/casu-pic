3ace7e350f73d227ce28aa6a53993884ceab342e
thaus <tomislav.haus@fer.hr>
2015-07-20 15:39:15 +0200

    Created mplab project named assisi-dspic-speaker

--- assisi-dspic-speaker.X/nbproject/Makefile-impl.mk
+++ assisi-dspic-speaker.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=assisi-dspic-main.X
+PROJECTNAME=assisi-dspic-speaker.X
 
 # Active Configuration
 DEFAULTCONF=default
